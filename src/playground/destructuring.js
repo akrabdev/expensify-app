3ffc7361af197c8f93b158af29c2d1396c638d7e
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-13 12:00:23 +0200

    Exploring ES6 Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,8 @@
+//
+// Object 
+//
+
+
 const Person = {
     name: 'Mike',
     age: 27,
@@ -14,3 +19,14 @@ const {city, temp: temperature } =  Person.location
 console.log(`My name is ${firstName}`);
 
 console.log(`temperature in ${city} is ${temperature}`)
+
+
+//
+// Array
+//
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '2.75'];
+
+const [itemName, ,meduimPrice] = item;
+
+console.log(`A meduim ${itemName} costs ${meduimPrice}`);
