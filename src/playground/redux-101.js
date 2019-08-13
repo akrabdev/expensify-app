6e9fb264bbe961be4eb1887a5bedd16efb382139
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-13 09:32:15 +0200

    Counter example: dispatching actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,11 +1,57 @@
 import {createStore} from 'redux';
 
-const store = createStore((state = {count: 0}) => {
-    return state;
+const store = createStore((state = {count: 0}, action) => {
+    console.log("createStore called");
+    switch (action.type) {
+        case 'INCREMENT': 
+            return {
+                count: state.count + 1
+            };
+
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+
+        case 'RESET':
+            return {
+                count: 0
+            };
+
+        default: return state;
+
+    }
 
 }) ;
 
+console.log(store.getState());
 
 
-console.log(store.getState()); 
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+console.log(store.getState());
+
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+console.log(store.getState());
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+console.log(store.getState());
+
+store.dispatch({
+    type: "RESET"
+});
+console.log(store.getState());
+
+store.dispatch({
+    type: "DECREMENT"});
+
+console.log(store.getState());
+
 
