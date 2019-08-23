d2f95a3ba5c823f4b53a36e37e7fe47184d331dc
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-23 20:23:11 +0200

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,61 @@
 const path = require("path");
 
-module.exports = { 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');// imported
+const CSSExtract = new ExtractTextPlugin('styles.css');//new instance
+// then we will tweak use array below
+//
+//we wont use style-loader as it handle inlinining of styles
+module.exports = (env) => {
+  const isProduction = env === 'production';  
+  return {
   
-  entry: './src/app.js', 
-   
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js' 
+    entry: './src/app.js', 
+     
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js' 
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+        },
+      {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+              sourceMap: true
+              }
+            },
+            {
+              loader: 'scss-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+            
+          ]
+        })
+      }]
+      
+      
+  
   },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-      },
-    {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-        
-      ]
-    }]
-    
-    
 
-},
-devtool: 'cheap-module--eval-source-map',
+  plugins: [
+    CSSExtract
+  ],
 
-devServer: {
-  contentBase: path.join(__dirname, 'public'),
-  historyApiFallback: true
-}
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
+  
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
+  }};
 };
+
 
