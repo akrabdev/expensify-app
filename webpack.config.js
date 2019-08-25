97e0dd01c6511741d2b0ce5efbce009aa3711dab
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-25 11:02:46 +0200

    setup dev-dependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = (env) => {
     entry: './src/app.js', 
      
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js' 
     },
     module: {
@@ -54,8 +54,9 @@ module.exports = (env) => {
   
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
+    historyApiFallback: true,
+    publicPath: '/dist/'
   }};
 };
 
-
+//devServer never writes assests , serves them virtually , right now devServer is looking for virtual assets.
