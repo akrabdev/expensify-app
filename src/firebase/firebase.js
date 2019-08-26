37873fd3a5508408f0ba8096731046db04069915
Abdallah Hossam <abdallahakrab98@gmail.com>
2019-08-26 22:30:11 +0200

    connected firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyACxAjVUHK6Fc-Li4frlGVcDvx_NRzlUAE",
+    authDomain: "expensify-6caa7.firebaseapp.com",
+    databaseURL: "https://expensify-6caa7.firebaseio.com",
+    projectId: "expensify-6caa7",
+    storageBucket: "",
+    messagingSenderId: "290786575335",
+    appId: "1:290786575335:web:2f7bab7373f76eb8"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
